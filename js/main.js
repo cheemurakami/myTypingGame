56c4849e9c411eabd076e8aeb2ea8fd58103d890
Chisato Murakami <kiwipom1031@gmail.com>
2020-09-17 15:06:42 -0700

    add basic functionality

--- js/main.js
+++ js/main.js
@@ -1,17 +1,46 @@
 'use strict';
 
 {
-  const word = "red";
+  const words = ["red", "blue", "pink"]
+  let word;
+  let location = 0;
+  let startTime;
+  let isPlaying = false;
+
+  const setWord = () => {
+    word = words.splice(Math.floor(Math.random()),1)[0];
+    target.textContent = word;
+    location = 0;
+  }
 
   const target = document.getElementById('target');
-  target.textContent = word;
-
-  let location = 0;
+  
+  document.addEventListener('click', () => {
+    if (isPlaying){
+      return;
+    }
+    isPlaying = true
+    if (isPlaying) {
+      setWord();
+      startTime = Date.now();
+    }
+    })
 
   document.addEventListener('keydown', e => {
-    if(e.key === word[location]){
+    if(e.key !== word[location]){
+      return;
+    }
       location++;
       target.textContent = "_".repeat(location) + word.substring(location)
+
+      if(location === word.length){
+        if (words.length === 0){
+          const elapsedTime = ((Date.now() -startTime) / 1000).toFixed(2);
+          const result = document.getElementById('result');
+          result.textContent = `Finished! ${elapsedTime} seconds!`
+        }
+        setWord();
+      }
     }
-  });
+  );
 }
