0034f79a005b72f2bc1fccfa05dd48ec13d1e69b
Chisato Murakami <kiwipom1031@gmail.com>
2020-09-18 12:13:35 -0700

    add timer display

--- js/main.js
+++ js/main.js
@@ -15,11 +15,14 @@
   let location = 0;
   let startTime;
   let isPlaying = false;
+  let timeOut;
 
   const target = document.getElementById("target");
   const targetJp = document.getElementById("targetJp");
   const message = document.getElementById("message");
   const inputField = document.getElementById("input");
+  const timer = document.getElementById("timer");
+  
 
   const setWord = () => {
     inputField.placeholder = "";
@@ -42,8 +45,26 @@
       setWord();
       setupTyping();
       startTime = Date.now();
+      displayElapsedTime();
     }
   });
+
+  const displayElapsedTime = () => {
+    let elapsedTime = ((Date.now() - startTime));
+    const d = new Date(elapsedTime);
+    let seconds = String(d.getSeconds()).padStart(2, '0');
+    let milliSeconds = String(d.getMilliseconds()).padStart(3, '0');
+    timer.textContent = `${seconds} : ${milliSeconds}`
+    
+    timeOut = setTimeout(() => {
+      displayElapsedTime();
+    }, 10)
+  }
+
+  const stopTimer = () => {
+    clearTimeout(timeOut);
+    timer.textContent = ""
+  }
 
   const setupTyping = () => {
     document.addEventListener("keyup", (e) => {
@@ -55,11 +76,13 @@
   
       if (location === word.length) {
         if (words.length === 0) {
-          const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2);
+
+          stopTimer();
+          const finishedTime = ((Date.now() - startTime) / 1000).toFixed(2);
           message.textContent = "";
           inputField.style.display = "none";
           const result = document.getElementById("result");
-          result.textContent = `Finished! ${elapsedTime} seconds!`;
+          result.textContent = `You win! ${finishedTime} seconds!`;
         }
         setWord();
       }
