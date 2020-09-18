b656c8f3548d98942ba3705faa3de5de1ba78c04
Chisato Murakami <kiwipom1031@gmail.com>
2020-09-17 22:02:18 -0700

    add styling

--- js/main.js
+++ js/main.js
@@ -1,68 +1,65 @@
-'use strict';
+"use strict";
 
 {
-  const words = ["red", "blue", "pink", "orange", "yellow", "green", "purple"]
+  const words = ["red", "blue", "pink", "orange", "yellow", "green", "purple"];
   const wordsObj = {
-    "red" : "あか",
-    "blue" : "あお",
-    "pink" : "ピンク",
-    "orange" : "オレンジ",
-    "yellow" : "きいろ",
-    "green" : "みどり",
-    "purple" : "むらさき",
-  }
+    red: "あか",
+    blue: "あお",
+    pink: "ピンク",
+    orange: "オレンジ",
+    yellow: "きいろ",
+    green: "みどり",
+    purple: "むらさき",
+  };
   let word;
   let location = 0;
   let startTime;
   let isPlaying = false;
-  
-  const target = document.getElementById('target');
-  const targetJp = document.getElementById('targetJp')
-  const message = document.getElementById('message')
-  const inputField = document.getElementById('input')
-  
-  const setWord = () => {
-    inputField.placeholder = ""
-    inputField.value = ""
 
-    word = words.splice(Math.floor(Math.random() * words.length),1)[0];
-    
-    location = 0;    
+  const target = document.getElementById("target");
+  const targetJp = document.getElementById("targetJp");
+  const message = document.getElementById("message");
+  const inputField = document.getElementById("input");
+
+  const setWord = () => {
+    inputField.placeholder = "";
+    inputField.value = "";
+
+    word = words.splice(Math.floor(Math.random() * words.length), 1)[0];
+
+    location = 0;
     target.textContent = word;
     targetJp.textContent = wordsObj[word];
-  }
+  };
 
-  document.addEventListener('click', () => {
-    if (isPlaying){
+  document.addEventListener("click", () => {
+    if (isPlaying) {
       return;
     }
-    isPlaying = true
+    isPlaying = true;
     if (isPlaying) {
-      message.textContent = "👇👇👇 Type here! 👇👇👇"
+      message.textContent = "👇👇👇 Type here! 👇👇👇";
       setWord();
       startTime = Date.now();
     }
-    })
+  });
 
-  document.addEventListener('keyup', e => {
-    if(e.key !== word[location]){
+  document.addEventListener("keyup", (e) => {
+    if (e.key !== word[location]) {
       return;
     }
-      location++;
-      target.textContent = "_".repeat(location) + word.substring(location)
+    location++;
+    target.textContent = "_".repeat(location) + word.substring(location);
 
-      
-      if(location === word.length){
-        if (words.length === 0){
-          const elapsedTime = ((Date.now() -startTime) / 1000).toFixed(2);
-          message.textContent = ""
-          inputField.style.display = "none";
-          const result = document.getElementById('result');
-          result.textContent = `Finished! ${elapsedTime} seconds!`
-        }
-        setWord();
+    if (location === word.length) {
+      if (words.length === 0) {
+        const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2);
+        message.textContent = "";
+        inputField.style.display = "none";
+        const result = document.getElementById("result");
+        result.textContent = `Finished! ${elapsedTime} seconds!`;
       }
+      setWord();
     }
-
-  );
+  });
 }
